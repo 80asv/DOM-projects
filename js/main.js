d9a0b4d3e4face15dfcd50fa2ef841f19e86304b
Andres Sanabria <sanabria80.andres@gmail.com>
2022-07-01 13:25:46 -0500

    scroll bar added

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ import lateralMenu from "./lateral-menu.js";
 import {contador, alarm} from "./clock.js";
 import {moveball} from "./keyboard.js";
 import {countdown} from "./countdown.js";
+import scrollButton from "./scroll.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     lateralMenu('.container-menu', '.btn-folded', '.container-logo', '.screen-menu__open', '.link-section');
     contador("#iniciar-reloj", "#detener-reloj", ".reloj", ".pause");
     alarm("assets/alarm.mp3","#iniciar-alarma", "#detener-alarma");
     countdown(".btn-date.iniciar", ".btn-date.reiniciar", ".days", ".hours", ".minutes", ".seconds", ".title-countdown");
+    scrollButton('#ir_arriba_boton', '.indicador_scroll');
 });
 
 
